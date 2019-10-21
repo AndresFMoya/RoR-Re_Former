f362c68371b783c0462368105287a123184cdad0
AndresFMoya <andresfmoya@gmail.com>
2019-10-21 11:08:31 -0500

    Add presence validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(username: "Example User", email: "user@example.com", password: "foobar")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "should not be valid" do
+    @user = User.new(username: "   ", email: "  ", password: "   ")
+    assert_not @user.valid?
+  end
+
 end
