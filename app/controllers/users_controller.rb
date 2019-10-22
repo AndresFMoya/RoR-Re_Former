b3977cdfa75b3a88b7277dcbd3a951e8f65b0121
AndresFMoya <andresfmoya@gmail.com>
2019-10-22 10:35:21 -0500

    Edit update action to redirect to edit user path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to new_user_path
+      redirect_to edit_user_path(@user)
     else
       render 'edit'
     end
